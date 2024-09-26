fa33dc1d59c7da9dd3f62ca8287c3377654f0d78
Enrique Gomes <enrique.pgomes@hotmail.com>
2024-09-26 15:54:58 -0300

    feat: implementa funcionalidade que adiciona veículo na lista

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -14,9 +14,14 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placa = Console.ReadLine();
+            while (string.IsNullOrWhiteSpace(placa) || string.IsNullOrEmpty(placa))
+            {
+                Console.WriteLine("Placa inválida. Digite novamente.");
+                placa = Console.ReadLine();
+            }
+            veiculos.Add(placa);
         }
 
         public void RemoverVeiculo()
